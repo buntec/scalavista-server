15a439bf5971ce9560d60543b7da232aa5aaecbc
Christoph Bunte <christophbunte@gmail.com>
2018-11-03 12:55:41 +0100

    initial commit

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "blah"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
+
+fork := true
+
+libraryDependencies += "org.scala-lang" % "scala-compiler" % "2.12.7"
+
+libraryDependencies += "org.scala-lang" % "scala-library" % "2.12.7"
+
+libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.12.7"
+
+
---
+++ src/main/scala/Sandbox.scala
@@ -0,0 +1,80 @@
+import scala.tools.nsc.Settings
+import scala.tools.nsc.interactive.{Global, Response}
+import scala.tools.nsc.reporters.{ConsoleReporter, StoreReporter}
+import scala.reflect.internal.util.Position
+import scala.reflect.internal.util._
+import scala.reflect.io.AbstractFile
+
+object Sandbox extends App {
+
+  trait Foo
+
+  val settings = new Settings()
+
+  settings.embeddedDefaults[Foo]
+  settings.usejavacp.value = true
+
+  val reporter = new StoreReporter
+  val compiler = new Global(settings, reporter)
+
+  //val file = compiler.newSourceFile(code)
+  val file = new BatchSourceFile(AbstractFile.getFile("code"))
+
+  val files = List(file)
+
+  val position = Position.offset(file, 60)
+  println(Position.formatMessage(position, "Position:", false))
+
+
+  def getTypeAt(files: List[SourceFile], position: Position): String = {
+
+    val res = new Response[Unit]
+    compiler.askReload(files, res)
+    res.get(1000)
+
+    val res2 = new Response[compiler.Tree]
+    compiler.askTypeAt(position, res2)
+    val treeOption = getResult(res2)
+
+    treeOption match {
+
+      case Some(tree) => compiler.ask(() => tree.children.map(c => c.toString).mkString("\n"))
+      case None => "failed to get type...."
+
+    }
+
+  }
+
+  println(getTypeAt(files, position))
+
+
+  //val res3 = new Response[List[compiler.Member]]
+  //compiler.askTypeCompletion(position, res3)
+  //printResult(res3)
+
+  //val res4 = new Response[List[compiler.Member]]
+  //compiler.askScopeCompletion(position, res4)
+  //printResult(res4)
+
+
+  def getResult[T](res: Response[T]): Option[T] = {
+    val TIMEOUT = 10000
+    if (!res.isComplete && !res.isCancelled) {
+      res.get(TIMEOUT.toLong) match {
+            case Some(Left(t)) => Some(t)
+            case Some(Right(ex)) =>
+              ex.printStackTrace()
+              println(ex)
+              None
+            case None =>
+              println("None")
+              None
+          }
+        } else {
+          None
+        }
+  }
+
+  
+
+}
